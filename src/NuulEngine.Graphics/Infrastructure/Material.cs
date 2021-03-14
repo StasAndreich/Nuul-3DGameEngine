920b819e8920318b07ab73a6729087e83742fff8
StasAndreich <smedyancev@gmail.com>
2021-03-14 21:48:51 +0300

    Add Material class

---
+++ src/NuulEngine.Graphics/Infrastructure/Material.cs
@@ -0,0 +1,27 @@
+﻿using NullEngine.Graphics.Infrastructure.Structs;
+using SharpDX;
+
+namespace NullEngine.Graphics.Infrastructure
+{
+    internal class Material
+    {
+        public Material(Texture texture, Vector3 emmisiveK,
+            Vector3 ambientK, Vector3 diffuseK,
+            Vector3 specularK, float specularPower)
+        {
+            MaterialProperties = new MaterialProperties
+            {
+                emmisiveK = emmisiveK,
+                ambientK = ambientK,
+                diffuseK = diffuseK,
+                specularK = specularK,
+                specularPower = specularPower
+            };
+            Texture = texture;
+        }
+
+        public MaterialProperties MaterialProperties { get; }
+
+        public Texture Texture { get; }
+    }
+}
