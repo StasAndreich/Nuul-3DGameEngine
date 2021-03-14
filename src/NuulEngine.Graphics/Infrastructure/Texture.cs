c1d3694e78847ef2d8aaeff70b8d02e2ef6a5ab6
StasAndreich <smedyancev@gmail.com>
2021-03-14 21:34:14 +0300

    Add Texture class

---
+++ src/NuulEngine.Graphics/Infrastructure/Texture.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SharpDX;
+using SharpDX.Direct3D11;
+
+namespace NullEngine.Graphics.Infrastructure
+{
+    internal class Texture : IDisposable
+    {
+        private bool _isDisposed;
+
+        private SamplerState _samplerState;
+        private ShaderResourceView _shaderResourceView;
+
+        public Texture(Texture2D textureData, int height, int width,
+            ShaderResourceView shaderResourceView, SamplerState samplerState)
+        {
+            TextureData = textureData;
+            Height = height;
+            Width = width;
+            _shaderResourceView = shaderResourceView;
+            _samplerState = samplerState;
+        }
+
+        public int Height { get; }
+
+        public int Width { get; }
+
+        public Texture2D TextureData { get; }
+
+        public SamplerState SamplerState { get => _samplerState; }
+
+        public ShaderResourceView ShaderResourceView { get => _shaderResourceView; }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    Utilities.Dispose(ref _samplerState);
+                    Utilities.Dispose(ref _shaderResourceView);
+                }
+
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
