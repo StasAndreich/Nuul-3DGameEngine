2be3a3227f3ab30f2a5cba9588f392a121c8fbff
StasAndreich <smedyancev@gmail.com>
2021-03-24 17:29:38 +0300

    Add IncludeHandler

---
+++ src/NuulEngine/Graphics/Infrastructure/Shaders/IncludeHandler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using SharpDX.D3DCompiler;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Para_1
+{
+    internal sealed class IncludeHandler : Include
+    {
+        private Stream _stream;
+
+        public IDisposable Shadow { get; set; }
+
+        public void Close(Stream stream)
+        {
+            _stream.Dispose();
+            _stream = null;
+        }
+
+        public Stream Open(IncludeType type, string fileName, Stream parentStream)
+        {
+            string path = Application.StartupPath;
+            FileInfo[] files = new DirectoryInfo(path)
+                .GetFiles(fileName, SearchOption.AllDirectories);
+            var fileStream = new FileStream(files[0].FullName, FileMode.Open);
+            return fileStream;
+        }
+
+        public void Dispose()
+        {
+            _stream?.Dispose();
+        }
+    }
+}
