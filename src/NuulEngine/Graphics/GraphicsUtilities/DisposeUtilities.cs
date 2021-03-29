e25a548373e8e760be5a395bb74bca550afb740f
Stasich Medyancev <42981972+StasAndreich@users.noreply.github.com>
2021-03-29 12:58:57 +0300

    Add DisposeUtilities class

---
+++ src/NuulEngine/Graphics/GraphicsUtilities/DisposeUtilities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NuulEngine.Graphics.GraphicsUtilities
+{
+    internal static class DisposeUtilities
+    {
+        public static void DisposeDictionaryElements<T>(Dictionary<string, T> dictionary)
+            where T : class, IDisposable
+        {
+            foreach (var value in dictionary.Values)
+            {
+                value.Dispose();
+            }
+
+            dictionary.Clear();
+        }
+
+        public static void DisposeListElements<T>(List<T> list)
+            where T : class, IDisposable
+        {
+            foreach (var element in list)
+            {
+                element.Dispose();
+            }
+
+            list.Clear();
+        }
+    }
+}
